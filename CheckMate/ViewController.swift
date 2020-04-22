c683f6c853a3355aa7b3fe5bf5ddb4b0148f578b
Sagar Ayi <ayisagar@gmail.com>
2020-04-22 16:34:38 -0400

    Initial Commit

---
+++ CheckMate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckMate
+//
+//  Created by Sagar Ayi on 4/22/20.
+//  Copyright © 2020 Random Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
