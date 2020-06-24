f5d4f8cb0d5d610407018288050da1db927cb59d
sagarayi <ayisagar@gmail.com>
2020-06-24 11:16:02 -0700

    Added Board empty class for future use

---
+++ CheckMate/Model/Board.swift
@@ -0,0 +1,13 @@
+//
+//  Board.swift
+//  CheckMate
+//
+//  Created by Sagar Madhu Ayi on 6/24/20.
+//  Copyright © 2020 Random Corp. All rights reserved.
+//
+
+import UIKit
+
+class Board: NSObject {
+
+}
