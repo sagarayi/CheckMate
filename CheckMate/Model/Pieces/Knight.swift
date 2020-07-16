a6894f327af34434a4f804ece5782523d2640337
sagarayi <ayisagar@gmail.com>
2020-07-16 10:03:22 -0700

    WIP - Knight

--- CheckMate/Model/Pieces/Knight.swift
+++ CheckMate/Model/Pieces/Knight.swift
@@ -16,7 +16,62 @@ class Knight: Piece {
 
        override func getLegalMoves() -> Set<Position> {
         var legalMoves: Set<Position> = Set()
-
+        
+        let currX = self.getCurrentPosition().xPos
+        let currY = self.getCurrentPosition().yPos
+        
+        // White's perspective but applies to black as well
+        // RIGHT-UP
+        var xPos = currX + 2
+        var yPos = currY - 1
+        if !self.isOutOfBounds(xPos: xPos, yPos: yPos) {
+            legalMoves.insert(Position.init(xPos: xPos, yPos: yPos))
+        }
+        
+        xPos = currX + 1
+        yPos = currY - 2
+        if !self.isOutOfBounds(xPos: xPos, yPos: yPos) {
+            legalMoves.insert(Position.init(xPos: xPos, yPos: yPos))
+        }
+        
+        //LEFT-UP
+        xPos = currX + 2
+        yPos = currY + 1
+        if !self.isOutOfBounds(xPos: xPos, yPos: yPos) {
+            legalMoves.insert(Position.init(xPos: xPos, yPos: yPos))
+        }
+        
+        xPos = currX + 1
+        yPos = currY + 2
+        if !self.isOutOfBounds(xPos: xPos, yPos: yPos) {
+            legalMoves.insert(Position.init(xPos: xPos, yPos: yPos))
+        }
+        
+        //LEFT-DOWN
+        xPos = currX - 2
+        yPos = currY + 1
+        if !self.isOutOfBounds(xPos: xPos, yPos: yPos) {
+            legalMoves.insert(Position.init(xPos: xPos, yPos: yPos))
+        }
+        
+        xPos = currX - 1
+        yPos = currY + 2
+        if !self.isOutOfBounds(xPos: xPos, yPos: yPos) {
+            legalMoves.insert(Position.init(xPos: xPos, yPos: yPos))
+        }
+        
+        //RIGHT-DOWN
+        xPos = currX - 2
+        yPos = currY - 1
+        if !self.isOutOfBounds(xPos: xPos, yPos: yPos) {
+            legalMoves.insert(Position.init(xPos: xPos, yPos: yPos))
+        }
+        
+        xPos = currX - 1
+        yPos = currY - 2
+        if !self.isOutOfBounds(xPos: xPos, yPos: yPos) {
+            legalMoves.insert(Position.init(xPos: xPos, yPos: yPos))
+        }
 
            return legalMoves
        }
